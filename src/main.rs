1aa392293dda11113e03428bf688fe3077dec8ea
David Wendt <dcrkid@yahoo.com>
2018-07-12 01:07:45 -0400

    Add option to overlay encoded image data into an existing file

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ extern crate num;
 
 mod asmimg;
 
-use argparse::{ArgumentParser, Store};
-use std::fs::File;
+use argparse::{ArgumentParser, Store, StoreFalse, StoreTrue};
+use std::fs::{OpenOptions};
 use std::io;
+use std::io::Seek;
 use asmimg::encoder::encode_grayscale_image;
 use asmimg::formats::interpret_indexed_format_name;
 
@@ -14,6 +15,8 @@ fn main() -> io::Result<()> {
     let mut input_filename = "".to_string();
     let mut output_filename = "".to_string();
     let mut format = "".to_string();
+    let mut truncatemode = true;
+    let mut offset = 0u64;
     
     {
         let mut ap = ArgumentParser::new();
@@ -23,6 +26,9 @@ fn main() -> io::Result<()> {
         ap.refer(&mut input_filename).add_argument("input", Store, "Name of the modern image file to convert.");
         ap.refer(&mut output_filename).add_argument("output", Store, "Where to store the converted image as.");
         ap.refer(&mut format).add_option(&["--format"], Store, "The format to convert the image into.");
+        ap.refer(&mut truncatemode).add_option(&["--overlay"], StoreFalse, "Overlay encoding result onto existing file. Negates --truncate.")
+                                   .add_option(&["--truncate"], StoreTrue, "Erases existing file (if any) before encoding. Negates --overlay.");
+        ap.refer(&mut offset).add_option(&["--offset"], Store, "Where to write data to within the target file.");
         
         ap.parse_args_or_exit();
     }
@@ -30,8 +36,16 @@ fn main() -> io::Result<()> {
     println!("Converting {} to {}", input_filename, output_filename);
     
     let img = image::open(input_filename).unwrap();
-    let mut bin = File::create(output_filename)?;
+    let mut bin = OpenOptions::new().write(true).create(true).truncate(truncatemode).open(output_filename)?;
     let fmt = interpret_indexed_format_name(&format).unwrap();
+    
+    let orig_length = bin.seek(io::SeekFrom::End(0))?;
+    if offset > orig_length {
+        //Seeking beyond the end of a file is implementation defined. Hence, we error out
+        return Err(io::Error::new(io::ErrorKind::UnexpectedEof, "Proposed offset length exceeds length of file."))
+    }
+    
+    bin.seek(io::SeekFrom::Start(offset))?;
     
     {
         encode_grayscale_image(fmt, &mut bin, &img)
